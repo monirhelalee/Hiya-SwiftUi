8e8ebc9edd979d877b1f8755be027cb75fd7cfce
monir <mdmonirhelale@gmail.com>
2026-01-04 21:20:12 +0600

    Initial Commit

---
+++ Hiya/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hiya
+//
+//  Created by monir on 1/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by monir on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
