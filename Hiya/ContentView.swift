d1c608861bd7f98f484bfd9c8f7d4a0c58aa8dbc
monir <mdmonirhelale@gmail.com>
2026-01-04 23:28:51 +0600

    add in device AI and use to show response from given prompt

--- Hiya/ContentView.swift
+++ Hiya/ContentView.swift
@@ -6,16 +6,58 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    private var largeLamguageModel = SystemLanguageModel.default
+    private var session = LanguageModelSession()
+    
+    @State private var response: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            
+            switch largeLamguageModel.availability{
+            case .available:
+                Text(response)
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .bold()
+            case .unavailable(.deviceNotEligible):
+                Text("Your Device is not eligible for Apple Inteligent.")
+            case .unavailable(.appleIntelligenceNotEnabled):
+                Text("Please enable Apple Intelligence in settings")
+            case .unavailable(.modelNotReady):
+                Text("Model is not ready yet.")
+            case .unavailable(_):
+                Text("The AI feature in unavailable for unknown reason.")
+            }
+            Spacer()
+                
+            Button{
+                Task{
+                    let prompt = "Say hi in fun way."
+                    
+                    do{
+                        let reply = try await session.respond(to: prompt)
+                        response = reply.content
+                    } catch{
+                        response = "Failed to get response: \(error.localizedDescription)"
+                    }
+                    
+                }
+            } label: {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding()
+            }
+            .buttonStyle(.borderedProminent)
+            .buttonSizing(.flexible)
+            .glassEffect(.regular.interactive())
         }
         .padding()
+        .tint(.purple)
     }
 }
 
